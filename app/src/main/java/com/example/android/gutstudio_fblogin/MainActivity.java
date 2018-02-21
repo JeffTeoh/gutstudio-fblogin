9dc5def64c30ce7e47b8ccc58385329aa682a335
JeffTeoh <jeffreyteoh4311@gmail.com>
2018-02-21 17:03:36 +0800

    Create basic activity for login button

--- app/src/main/java/com/example/android/gutstudio_fblogin/MainActivity.java
+++ app/src/main/java/com/example/android/gutstudio_fblogin/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.gutstudio_fblogin;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void loginpage (View view) {
+        Intent intent = new Intent(this, loginpage.class);
+        Button bLogin = (Button) findViewById(R.id.bLogin);
+        startActivity(intent);
     }
 }
